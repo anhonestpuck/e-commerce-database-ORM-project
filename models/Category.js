5324923ac5051e8517a7c70a310d580a9a3006cb
Jeff Pearson <pjeffrey40@gmail.com>
2022-09-21 16:43:47 -0400

    second commit

--- models/Category.js
+++ models/Category.js
@@ -3,10 +3,22 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+//started with my tutor by listing out all the table value definitions
 Category.init(
   {
-    // define columns
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+
+   },
+
+   
   },
   {
     sequelize,
