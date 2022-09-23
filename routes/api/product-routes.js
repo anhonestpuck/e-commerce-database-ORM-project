0e8316121e386a7a5a3e1fd7dac45319b1bfb373
Jeff Pearson <pjeffrey40@gmail.com>
2022-09-23 10:54:53 -0400

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category},{model: Tag}]
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      include: [{model: Category}, {model: Tag}],
+    });
+
+    if (!productData) {
+      res.status(404).json({message: 'no user with this id'})
+    }
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
